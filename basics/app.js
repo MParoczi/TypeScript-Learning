c337bcb3fc8020a2e2d14dc95454c2a5ee4886a4
Márk Paróczi <mark.paroczi@gmail.com>
2020-07-28 08:49:08 +0200

    Add different kind of arrays to app.ts

--- basics/app.js
+++ basics/app.js
@@ -1,5 +1,12 @@
 var person = {
     name: "Mark",
-    age: 25
+    age: 25,
+    hobbies: ["Sports", "Cooking"]
 };
+var favoriteActivities;
+favoriteActivities = ["Sports"];
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
 console.log(person.name);
--- basics/app.ts
+++ basics/app.ts
@@ -1,9 +1,18 @@
 const person: {
   name: string;
   age: number;
+  hobbies: string[];
 } = {
   name: "Mark",
-  age: 25
+  age: 25,
+  hobbies: ["Sports", "Cooking"]
 };
+
+let favoriteActivities: any[];
+favoriteActivities = ["Sports"];
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
 
 console.log(person.name);
