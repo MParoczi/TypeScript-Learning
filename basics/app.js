f2cf55774373af2a44cd87b63349e475301f9cdf
Márk Paróczi <mark.paroczi@gmail.com>
2020-07-28 09:12:24 +0200

    Add enum to app.ts

--- basics/app.js
+++ basics/app.js
@@ -1,8 +1,15 @@
+var Authorization;
+(function (Authorization) {
+    Authorization[Authorization["ADMIN"] = 0] = "ADMIN";
+    Authorization[Authorization["READ_ONLY"] = 1] = "READ_ONLY";
+    Authorization[Authorization["AUTHOR"] = 2] = "AUTHOR";
+})(Authorization || (Authorization = {}));
 var person = {
     name: "Mark",
     age: 25,
     hobbies: ["Sports", "Cooking"],
-    role: [2, "author"]
+    role: [2, "author"],
+    authorization: Authorization.ADMIN
 };
 // person.role.push("admin");
 // person.role[0] = 10;
--- basics/app.ts
+++ basics/app.ts
@@ -1,13 +1,21 @@
+enum Authorization {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR
+}
+
 const person: {
   name: string;
   age: number;
   hobbies: string[];
   role: [number, string];
+  authorization: number;
 } = {
   name: "Mark",
   age: 25,
   hobbies: ["Sports", "Cooking"],
-  role: [2, "author"]
+  role: [2, "author"],
+  authorization: Authorization.ADMIN
 };
 
 // person.role.push("admin");
